bc2b4ac7e089f61c4aa310820869ae3d70690235
Niklas Schuerrle <niklas733@correo.ugr.es>
2025-10-28 18:59:31 +0100

    remove static html file and change in db config

--- expense_tracker/core/db.py
+++ expense_tracker/core/db.py
@@ -1,8 +1,8 @@
 from sqlmodel import SQLModel, create_engine, Session
 
-ENGINE_URL = "sqlite:///./dev.db"
-engine = create_engine(ENGINE_URL, echo=False)
-
+BASE_DIR = Path(__file__).resolve().parent  # -> expense_tracker/
+DB_PATH = BASE_DIR / "dev.db"
+engine = create_engine(f"sqlite:///{DB_PATH}", echo=False)
 
 def init_db() -> None:
     SQLModel.metadata.create_all(engine)
